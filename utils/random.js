d7247c5add4581d2b11d913dd9a4b0a7f902d154
LMOliver <1779180506@qq.com>
2019-10-08 15:59:56 +0800

    forty backend

--- utils/random.js
+++ utils/random.js
@@ -1,3 +1,6 @@
 module.exports.randomInteger=function randomInteger(max){
 	return Math.floor(Math.random()*max);
 };
+module.exports.randomReal=function randomReal(min,max){
+	return min+Math.random()*(max-min);
+}
