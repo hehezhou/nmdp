bd3fd1dbdb088fffcb70670dee2b8b601aa3b3f3
hehezhou <51103289+hehezhou@users.noreply.github.com>
2019-10-08 10:48:03 +0800

    hehezhou

--- public/forty/core.js
+++ public/forty/core.js
@@ -1,17 +1,99 @@
-class GAME {
-    constructor({ data }) {
-
+const GAME = (() => {
+    const
+        PLAYER_TIME_BEFORE_ATTACK = 1,
+        PLAYER_MAX_SPEED = 30,
+        PLAYER_ACC = 120,
+        PLAYER_MAX_HEALTH = 100,
+        PLAYER_ATTACK_RANGE = 40,
+        PLAYER_ATTACK_ANGLE = Math.PI / 6,
+        PLAYER_ATTACK_DAMAGE = 75,
+        PLAYER_ATTACK_HEAL = 25,
+        PLAYER_HURT_PER_SEC = 40,
+        ARENA_HEIGHT = 1000,
+        ARENA_WIDTH = 1000;
+    class vector {
+        constructor(x, y) {
+            if(typeof x === 'number') {
+                this.x = x, this.y = y;
+            }
+            else {
+                this.x = x.x;
+                this.y = x.y;
+            }
+        }
+        len() {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        }
+        angle() {
+            let ans = Math.atan2(y, x);
+            if(ans < 0) ans += 2 * Math.PI;
+            return ans;
+        }
+    };
+    function plus(a, b) {return new vector(a.x + b.x, a.y + b.y);}
+    function sub(a, b) {return new vector(a.x - b.x, a.y - b.y);}
+    function mult(a, b) {return new vector(a.x * b, a.y * b);}
+    function div(a, b) {return new vector(a.x / b, a.y / b);}
+    function angleToVector(angle) {
+        return new vector(cos(angle), sin(angle));
     }
-    update(data) {
-
+    let Waiting = Symbol('waiting');
+    class BeforeAttack {
+        constructor({time, angle}) {
+            this.time = time;
+            this.angle = angle;
+        }
     }
-    getNowMap() {
-
+    function mid(a, b, c) {
+        if(a < b) a ^= b ^= a ^= b;
+        if(b < c) b ^= c ^= b ^= c;
+        if(a < b) a ^= b ^= a ^= b;
+        return b;
     }
-    check({ x, y }) {
-
+    class Player {
+        /**
+         * @param {{pos: vector, speed: vector, targetSpeed: vector, attackState: Waiting|BeforeAttack, health: number, targetHealth: number}} param0 
+         */
+        constructor({pos, speed, targetSpeed, attackState, health, targetHealth}) {
+            this.pos = pos;
+            this.speed = speed;
+            this.targetSpeed = targetSpeed;
+            this.attackState = attackState;
+            this.health = health;
+            this.targetHealth = targetHealth;
+        }
+        time(t) {
+            let deltaSpeed = sub(this.targetSpeed - this.speed);
+            let dl = deltaSpeed.len();
+            let p = Math.max(dl / PLAYER_ACC, t);
+            this.pos = plus(pos, div(mult(deltaSpeed, p), 2));
+            this.speed = plus(this.speed, mult(deltaSpeed, p / deltaSpeed.len()));
+            this.pos = plus(this.pos, mult(speed, (t - p / 2)));
+            this.pos.x = mid(0, this.pos.x, ARENA_WIDTH);
+            this.pos.y = mid(0, this.pos.y, ARENA_HEIGHT);
+            if(this.attackState instanceof BeforeAttack) {
+                this.attackState.time -= t;
+                if(this.attackState.time < 0) this.attackState = Waiting;
+            }
+            this.health = max(this.targetHealth, this.health - PLAYER_HURT_PER_SEC * t);
+        }
     }
-    ranking() {
+    let players = new Set();
+    return class {
+        constructor({ data }) {
 
+        }
+        update(data) {
+
+        }
+        getNowMap() {
+
+        }
+        check({ x, y }) {
+
+        }
+        ranking() {
+
+        }
     }
-}
+})();
