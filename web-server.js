7536769cdf2329ef635999642188f88c781e7feb
hehezhou <596082417@qq.com>
2020-11-21 16:19:03 +0800

    fix bug

--- web-server.js
+++ web-server.js
@@ -9,7 +9,7 @@ const ableList = [
 	/^\/?$/,
 	/^(?:\/[a-zA-Z_0-9\-]+)*(?:\/[a-zA-Z_0-9\-]+\.[a-zA-Z_0-9\-]+)?$/,
 ];
-const regexp1 = /^\/(paint-board|area-single|mosiyuan|area|atom|forty|login)$/, regexp2 = /^\/(((paint-board|area-single|mosiyuan|area|atom|forty|login)\/)?)?$/;
+const regexp1 = /^\/(paint-board|area-single|mosiyuan|area|atom|forty|login|account)$/, regexp2 = /^\/(((paint-board|area-single|mosiyuan|area|atom|forty|login|account)\/)?)?$/;
 const typeList = {
 	'.css': { 'Content-Type': 'text/css;charset=utf-8' },
 	'.html': { 'Content-Type': 'text/html;charset=utf-8' },
